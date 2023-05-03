fbdf8f2fe119b6a6883fa08163d5b7a31f3dff9e
Eduardo <estgv19172@alunos.estgv.ipv.pt>
2023-05-03 17:10:15 +0100

    Update Procura_Sofrega.py

--- Procura_Sofrega.py
+++ Procura_Sofrega.py
@@ -4,20 +4,25 @@ def encontrarMenorKM (cidadesParaExpandir):
     distancia = cidadesParaExpandir[0]['faro']
     cidade = cidadesParaExpandir[0]
 
+
+
     for candidato in cidadesParaExpandir:
         if (candidato['faro'] < distancia):
             distancia = candidato['faro']
             cidade = candidato
 
+   
     return cidade
 
 def procuraSofrega (mapa, inicio, destino):
 
     cidadesParaExpandir = []
+    caminho = []
+    distancia = 0
 
     # adicionamos o ponto de partida
     cidadesParaExpandir.append (
-        {'cidade': inicio.nomeCidade, 'historicoCaminho': [inicio.nomeCidade], 'faro' : inicio.distanciaParaFaro}
+        {'cidade': inicio.nomeCidade, 'faro' : inicio.distanciaParaFaro}
     )
 
     while True:
@@ -27,18 +32,22 @@ def procuraSofrega (mapa, inicio, destino):
         
         # verificar se e o destino
         if (proximoNoExpandir['cidade'] == destino.nomeCidade):
-            print(proximoNoExpandir['historicoCaminho'])
+            caminho.append(destino.nomeCidade)
+            print(caminho)
             break
+
+        # remover o no que foi totalmente expandido
+        cidadesParaExpandir.clear()
+
+        caminho.append(proximoNoExpandir['cidade'])
 
         # adicionar os vizinhos do no que foi expandido
         cidade = mapa.obterCidade(proximoNoExpandir['cidade'])
         for vizinho in cidade.vizinhos:
             cidadesParaExpandir.append({
                     'cidade': vizinho,
-                    'historicoCaminho': proximoNoExpandir['historicoCaminho'] + [vizinho], 
-                    'faro' : inicio.distanciaParaFaro
+                    'faro' : cidade.vizinhos[vizinho]['cidade'].distanciaParaFaro
                 }
             )
 
-        # remover o no que foi totalmente expandido
-        cidadesParaExpandir.remove(proximoNoExpandir)
+        
