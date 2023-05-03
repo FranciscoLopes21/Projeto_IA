e21975e3f646c8d4e735d151cfce25d63818d454
Francisco Lopes <85237841+FranciscoLopes21@users.noreply.github.com>
2023-05-03 21:43:24 +0100

    try-except

--- Procura_Sofrega.py
+++ Procura_Sofrega.py
@@ -20,28 +20,33 @@ def procuraSofrega (mapa, inicio, destino):
         {'cidade': inicio.nomeCidade, 'historicoCaminho': [inicio.nomeCidade],'distancia': 0, 'faro' : inicio.distanciaParaFaro}
     )
 
-    while True:
+    try:
 
-        # encontrar o proximo no a expandir
-        proximoNoExpandir = encontrarMenorKM(cidadesParaExpandir)
-        
-        # verificar se e o destino
-        if (proximoNoExpandir['cidade'] == destino.nomeCidade):
-            print(proximoNoExpandir['historicoCaminho'])
-            print(proximoNoExpandir['distancia'], "kms")
-            break
+        while True:
 
-        # adicionar os vizinhos do no que foi expandido
-        # distancia entre as cidades
-        cidade = mapa.obterCidade(proximoNoExpandir['cidade'])
-        for vizinho in cidade.vizinhos:
-            cidadesParaExpandir.append({
-                    'cidade': vizinho,
-                    'historicoCaminho': proximoNoExpandir['historicoCaminho'] + [vizinho],
-                    'distancia': cidade.vizinhos[vizinho]['distancia'] + proximoNoExpandir['distancia'],
-                    'faro' : cidade.vizinhos[vizinho]['cidade'].distanciaParaFaro
-                }
-            )
+            # encontrar o proximo no a expandir
+            proximoNoExpandir = encontrarMenorKM(cidadesParaExpandir)
+            
+            # verificar se e o destino
+            if (proximoNoExpandir['cidade'] == destino.nomeCidade):
+                print(proximoNoExpandir['historicoCaminho'])
+                print(proximoNoExpandir['distancia'], "kms")
+                break
 
-        # remover o no que foi totalmente expandido
-        cidadesParaExpandir.remove(proximoNoExpandir)
+            # adicionar os vizinhos do no que foi expandido
+            # distancia entre as cidades
+            cidade = mapa.obterCidade(proximoNoExpandir['cidade'])
+            for vizinho in cidade.vizinhos:
+                cidadesParaExpandir.append({
+                        'cidade': vizinho,
+                        'historicoCaminho': proximoNoExpandir['historicoCaminho'] + [vizinho],
+                        'distancia': cidade.vizinhos[vizinho]['distancia'] + proximoNoExpandir['distancia'],
+                        'faro' : cidade.vizinhos[vizinho]['cidade'].distanciaParaFaro
+                    }
+                )
+
+            # remover o no que foi totalmente expandido
+            cidadesParaExpandir.remove(proximoNoExpandir)
+
+    except:
+        print("Ocorreu um erro ao encontrar caminho.")
