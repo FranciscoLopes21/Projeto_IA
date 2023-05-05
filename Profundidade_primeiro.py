25dd5e2c8e7eb958e51bf286b03823f495989102
Eduardo <estgv19172@alunos.estgv.ipv.pt>
2023-05-05 18:42:05 +0100

    prof primeiro

--- Profundidade_primeiro.py
+++ Profundidade_primeiro.py
@@ -1,26 +1,43 @@
-import Mapa as mapa
+def profundidadePrimeiro(mapa, inicio, destino):
+    cidadesParaExpandir = []
+    cidadesJaVisitadas = []
 
-def profundidadePrimeiro(mapa, inicio, destino,visitado=None,caminho=None):
-    if visitado is None:
-        visitado = set()
-    if caminho is None:
-        caminho= []
-    visitado.add(inicio)
-    caminho=caminho + [inicio]
-    if inicio == destino:
-        return caminho
-    print('caminho:',caminho)
-    for vizinho in mapa[inicio]:
-        if vizinho not in visitado:
-            novo_caminho = profundidadePrimeiro(mapa,vizinho,destino,visitado,caminho)
-            if novo_caminho:
-                return novo_caminho
-    return []
+    # adicionamos o ponto de partida a pilha
+    cidadesParaExpandir.append (
+        {'cidade': inicio.nomeCidade, 'distancia': 0, 'historicoCaminho': [inicio.nomeCidade]}
+    )
 
- #teste
-inicio = 'Porto'
-destino = 'Castelo Branco'
-caminho = profundidadePrimeiro(mapa, inicio, destino)
-print('Caminho encontrado:', caminho)
-             
+    numeroIteracoes = 0 
 
+    while numeroIteracoes <= 150:
+
+        if (len(cidadesParaExpandir) == 0):
+            print("Nao foi possivel encontrar o caminho")
+            return
+
+        proximoNoExpandir = cidadesParaExpandir.pop(0)
+        
+        # adicionamos a cidade ja visitada numa lista, de modo a evitar futuros loops
+        cidadesJaVisitadas.append(proximoNoExpandir['cidade'])
+
+        cidade = mapa.obterCidade(proximoNoExpandir['cidade'])
+        for vizinho in cidade.vizinhos:
+            # verificar se o no destino
+            if (vizinho == destino.nomeCidade):
+                print(proximoNoExpandir['historicoCaminho'] + [vizinho])
+                print(proximoNoExpandir['distancia'] + cidade.vizinhos[vizinho]['distancia'])
+                print(numeroIteracoes)
+                return
+        
+            # caso nao seja adicionamos os novos vizinhos a lista
+            if vizinho not in cidadesJaVisitadas:
+                cidadesParaExpandir.append({
+                    'cidade': vizinho,
+                    'distancia': cidade.vizinhos[vizinho]['distancia'] + proximoNoExpandir['distancia'],
+                    'historicoCaminho': proximoNoExpandir['historicoCaminho'] + [vizinho]
+                })
+        
+        numeroIteracoes += 1
+        
+    
+    print("crashhhhhh")
