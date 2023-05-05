64f8e0c9e607f6d620032ecdec19e0ff94542774
Eduardo <estgv19172@alunos.estgv.ipv.pt>
2023-05-05 21:25:10 +0100

    pp

--- Profundidade_primeiro.py
+++ Profundidade_primeiro.py
@@ -7,36 +7,30 @@ def profundidadePrimeiro(mapa, inicio, destino):
         {'cidade': inicio.nomeCidade, 'distancia': 0, 'historicoCaminho': [inicio.nomeCidade]}
     )
 
-    numeroIteracoes = 0 
+    numeroIteracoes = 0
 
-    while numeroIteracoes <= 150:
+    while numeroIteracoes <= 500:
 
         if (len(cidadesParaExpandir) == 0):
             print("Nao foi possivel encontrar o caminho")
             return
 
         proximoNoExpandir = cidadesParaExpandir.pop(0)
-        
-        # adicionamos a cidade ja visitada numa lista, de modo a evitar futuros loops
-        cidadesJaVisitadas.append(proximoNoExpandir['cidade'])
+
+        # verificar se o no destino
+        if (proximoNoExpandir['cidade'] == destino.nomeCidade):
+            print(proximoNoExpandir['historicoCaminho'])
+            print(proximoNoExpandir['distancia'])
+            return
 
         cidade = mapa.obterCidade(proximoNoExpandir['cidade'])
         for vizinho in cidade.vizinhos:
-            # verificar se o no destino
-            if (vizinho == destino.nomeCidade):
-                print(proximoNoExpandir['historicoCaminho'] + [vizinho])
-                print(proximoNoExpandir['distancia'] + cidade.vizinhos[vizinho]['distancia'])
-                print(numeroIteracoes)
-                return
-        
             # caso nao seja adicionamos os novos vizinhos a lista
-            if vizinho not in cidadesJaVisitadas:
-                cidadesParaExpandir.append({
-                    'cidade': vizinho,
-                    'distancia': cidade.vizinhos[vizinho]['distancia'] + proximoNoExpandir['distancia'],
-                    'historicoCaminho': proximoNoExpandir['historicoCaminho'] + [vizinho]
-                })
-        
+            cidadesParaExpandir.insert(0,{
+                'cidade': vizinho,
+                'distancia': cidade.vizinhos[vizinho]['distancia'] + proximoNoExpandir['distancia'],
+                'historicoCaminho': proximoNoExpandir['historicoCaminho'] + [vizinho]
+            })
         numeroIteracoes += 1
         
     
